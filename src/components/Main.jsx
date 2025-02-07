ffc5a5922a15aa1c1eddec72e8b7f7dd3ac949e2
Rafael Andrade <devrafaelandradeoliveira@gmail.com>
2025-02-07 13:47:36 -0300

    Add: Estilização das tarefas

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,33 +2,65 @@ import { Component } from "react";
 import './Main.css'
 
 //Form
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
 
-export default class Vain extends Component{
+export default class Vain extends Component {
   state = {
     novaTarefa: '',
+    tarefas: [
+      'Fazer café',
+      'Beber água',
+      'Estudar',
+    ],
   };
 
   inputMudou = (e) => {
     this.setState({
-        novaTarefa: e.target.value,
+      novaTarefa: e.target.value,
+    });
+  };
+
+  adicionarTarefa = (e) => {
+    e.preventDefault();
+    const { novaTarefa, tarefas } = this.state;
+
+    if (novaTarefa.trim() !== '') {
+      this.setState({
+        tarefas: [...tarefas, novaTarefa],
+        novaTarefa: '',
       });
-  }
+    }
+  };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
-        <form action="#" className="form">
-          <input onChange={this.inputMudou} type="text" value={novaTarefa}/>
+        <form action="#" className="form" onSubmit={this.adicionarTarefa}>
+          <input
+            onChange={this.inputMudou}
+            type="text"
+            value={novaTarefa}
+            placeholder="Adicione uma tarefa"
+          />
           <button type="submit">
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa, index) => (
+            <li key={index}>{tarefa}
+              <div>
+                <FaEdit className="edit"/>
+                <FaWindowClose className="delete"/>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
-
